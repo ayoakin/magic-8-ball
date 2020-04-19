b3158b887c0aa181da4e1fa64c0cf1beccbea509
Ayo <56697156+ayoakin@users.noreply.github.com>
2020-04-19 17:11:51 -0400

    Update and rename README.md to magic8.js

---
+++ magic8.js
@@ -0,0 +1,51 @@
+# magic-8-ball
+
+let userName = 'Ayo';
+
+userName ? console.log('Hello $userName') : console.log('Hello')
+
+let userQuestion = 'How does the future look?'
+
+console.log(`${userName} asked: ${userQuestion}`);
+
+let randomNumber = Math.floor(Math.random() * 8);
+
+let eightBall = '';
+
+switch(randomNumber){
+    
+  case 0:
+    eightBall = 'It is certain'
+    break;
+    
+  case 1:
+    eightBall = 'It is decidely so'
+    break;
+    
+  case 2:
+    eightBall = 'Reply hazy try again'
+    break;
+    
+  case 3:
+    eightBall = 'Cannot predict now'
+    break;
+    
+  case 4: 
+    eightBall = 'Do not count on it'
+    break;
+    
+  case 5: 
+    eightBall = 'My sources say no'
+    break;
+    
+  case 6:
+    eightBall = 'Outlook not so good'
+    break;
+    
+  case 7: 
+    eightBall = 'Signs point North'
+    break;
+    
+}
+
+console.log(eightBall);
